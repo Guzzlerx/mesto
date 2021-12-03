eb03a95e3efbc2c75a29c581eb58f08d8c8e4ca0
Andrew Pasynkov <pasynkov2012@yandex.ru>
2021-12-03 23:35:45 +0300

    2nd commit

--- script/script.js
+++ script/script.js
@@ -1,31 +1,30 @@
 let content = document.querySelector('.content');
-let buttonEditProfile = content.querySelector('.profile__button-edit-info');
-let popup = document.querySelector('.popup');
-let buttonCloseProfile = popup.querySelector('.popup__button-close');
-
-function toggleClass () {
-    popup.classList.toggle('popup_off')
-}
-
-buttonEditProfile.addEventListener('click', toggleClass)
-buttonCloseProfile.addEventListener('click', toggleClass)
-
-let formProfile = document.querySelector('.popup__container')
+let buttonEditProfile = content.querySelector('.profile__button-edit-info');  // кнопка редактирования профиля
+let popup = document.querySelector('.popup');   // выбор модалки
+let buttonCloseProfile = popup.querySelector('.popup__button-close');  // кнопка закрытия попапа
+let formProfile = document.querySelector('.popup__container')   // форма профиля
 let profileName = content.querySelector('.profile__name');
 let profileDescription = content.querySelector('.profile__description');
 let inputName = formProfile.querySelector('.popup__input_type_name');
 let inputDescription = formProfile.querySelector('.popup__input_type_description');
 
-// inputName.setAttribute('value', `${profileName.textContent}`);
-// inputDescription.setAttribute('value', `${profileDescription.textContent}`);
-
-inputName.value = profileName.textContent;
-inputDescription.value = profileDescription.textContent;
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    profileName.textContent = inputName.value;
-    profileDescription.textContent = inputDescription.value;
+function openPopup () {   //  открываем попап и заполняем инпуты данными из профиля
+    popup.classList.add('popup_active')
+    inputName.value = profileName.textContent;
+    inputDescription.value = profileDescription.textContent;
 }
 
-formProfile.addEventListener('submit', formSubmitHandler);
+function closePopup () {    // закрываем попап
+    popup.classList.remove('popup_active')
+}
+
+function formSubmitHandler (evt) {  // нажимаем на кнопку сохранить и данные из инпутов сохраняются
+    evt.preventDefault();           // в профиле, после чего попап закрывается
+    profileName.textContent = inputName.value;
+    profileDescription.textContent = inputDescription.value;
+    closePopup()
+}
+
+buttonEditProfile.addEventListener('click', openPopup)
+buttonCloseProfile.addEventListener('click', closePopup)
+formProfile.addEventListener('submit', formSubmitHandler);  // при нажатии на кнопку формы с типом submit (отправки формы)
