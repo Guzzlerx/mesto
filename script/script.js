78cd3acb6ac44f769eec23953da57caf55df285b
Andrew Pasynkov <pasynkov2012@yandex.ru>
2021-12-13 00:50:56 +0300

    add_6_cards_by_JS

--- script/script.js
+++ script/script.js
@@ -28,3 +28,41 @@ function formSubmitHandler (evt) {  // нажимаем на кнопку сох
 buttonEditProfile.addEventListener('click', openPopup)
 buttonCloseProfile.addEventListener('click', closePopup)
 formProfile.addEventListener('submit', formSubmitHandler);  // при нажатии на кнопку формы с типом submit (отправки формы)
+
+const initialCards = [
+    {
+        name: 'Sheeps',
+        link: './images/there_are_more_sheep_than_people.jpg'
+    },
+    {
+        name: 'Night sky',
+        link: './images/night_sky_at_china.jpg'
+    },
+    {
+        name: 'A small model',
+        link: './images/a_very_small_model.jpg'
+    },
+    {
+        name: 'Quiet woods',
+        link: './images/a_walk_in_the_quiet_woods.jpg'
+    },
+    {
+        name: 'Salmon River',
+        link: './images/old_salmon_river.jpg'
+    },
+    {
+        name: 'A lonely island',
+        link: './images/the_lonely_island.jpg'
+    }
+];
+const cardsContainer = document.querySelector('.cards__grid-list');  // ul для карточек
+const cardsTemplate = document.querySelector('.cards-template').content;    //  template card(li)
+
+function addCard(item) {
+    let cardsItem = cardsTemplate.querySelector('.cards__grid-item').cloneNode(true); // при добавлении нового эл-та
+    cardsItem.querySelector('.cards__title').textContent = item.name;                     // обязательно снова клонировать
+    cardsItem.querySelector('.cards__photo').src = item.link;
+    cardsContainer.append(cardsItem);
+}
+
+initialCards.forEach(addCard);
