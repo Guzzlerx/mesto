f4ddbfd33488eef3d2727e4e696b4ee86666847e
Andrew Pasynkov <pasynkov2012@yandex.ru>
2021-12-14 22:26:51 +0300

    finish work

--- script/script.js
+++ script/script.js
@@ -3,8 +3,10 @@ const buttonEditProfile = content.querySelector('.profile__button-edit-info');  
 const buttonNewCard = content.querySelector('.profile__button-add-picture'); // кнопка добавления карточки
 const popupProfile = document.querySelector('.popup_type_edit-profile');   // выбор модалки профиля
 const popupNewCard = document.querySelector('.popup_type_add-card');    // выбор модалки новой карточки
+const popupZoomPhoto = document.querySelector('.popup_type_zoom-photo');
 const buttonCloseProfile = document.querySelector('.popup__button-close_type_edit-profile');  // кнопка закрытия попапа профиля
 const buttonCloseNewCard = document.querySelector('.popup__button-close_type_add-card');    // кнопка закрытия попапа доб. карточки
+const buttonCloseZoom = document.querySelector('.popup__button-close_type_zoom-photo')
 const formProfile = document.querySelector('.popup__form_type_edit-profile');   // форма профиля
 const formNewCard = document.querySelector('.popup__form_type_add-card');
 const profileName = content.querySelector('.profile__name');
@@ -52,6 +54,21 @@ function createCard(item) {
     buttonLike.addEventListener('click', () => {
         buttonLike.classList.toggle('cards__like_active');
     })
+
+    const buttonRemove = cardsItem.querySelector('.cards__trash');
+    buttonRemove.addEventListener('click', () => {
+        buttonRemove.closest('.cards__grid-item').remove()
+    })
+
+    const buttonZoom = cardsItem.querySelector('.cards__photo');
+    const titleZoom = cardsItem.querySelector('.cards__title');
+    buttonZoom.addEventListener('click', () => {
+        popupZoomPhoto.querySelector('.popup__photo').src = buttonZoom.src;
+        popupZoomPhoto.querySelector('.popup__photo').classList.add('popup__photo_active');
+        popupZoomPhoto.querySelector('.popup__title-zoom').textContent = titleZoom.textContent;
+        openPopupZoomPhoto();
+    })
+
     return cardsItem;
 }
 
@@ -65,6 +82,7 @@ function addNewCard(item) {
 
 function openPopup(popup) {
     popup.classList.add('popup_active');
+    document.querySelector('.body').classList.add('body_scroll-off')
 }
 
 function openPopupProfile () {   //  открываем попап и заполняем инпуты данными из профиля
@@ -77,8 +95,13 @@ function openPopupNewCard () {   //  открываем попап и запол
     openPopup(popupNewCard);
 }
 
+function openPopupZoomPhoto () {   //  открываем попап и заполняем инпуты данными из профиля
+    openPopup(popupZoomPhoto);
+}
+
 function closePopup (popup) {    // закрываем попап
     popup.classList.remove('popup_active');
+    document.querySelector('.body').classList.remove('body_scroll-off')
 }
 
 function closePopupProfile () {
@@ -89,6 +112,10 @@ function closePopupNewCard () {
     closePopup(popupNewCard);
     inputPlace.value = '';
     inputLink.value = '';
+}
+
+function closePopupZoom () {
+    closePopup(popupZoomPhoto);
 }
 
 function formSubmitHandlerProfile (evt) {  // нажимаем на кнопку сохранить и данные из инпутов сохраняются
@@ -116,4 +143,4 @@ formProfile.addEventListener('submit', formSubmitHandlerProfile);  // при н�
 formNewCard.addEventListener('submit', formSubmitHandlerNewCard);
 buttonCloseProfile.addEventListener('click', closePopupProfile);
 buttonCloseNewCard.addEventListener('click', closePopupNewCard);
-
+buttonCloseZoom.addEventListener('click', closePopupZoom);
