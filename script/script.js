62bc6679c714e2fc4d3d351f9a49fd542adaff46
Andrew Pasynkov <pasynkov2012@yandex.ru>
2021-12-16 21:03:37 +0300

    fix mistakes after 1st review

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ const popupNewCard = document.querySelector('.popup_type_add-card');    // вы�
 const popupZoomPhoto = document.querySelector('.popup_type_zoom-photo');
 const buttonCloseProfile = document.querySelector('.popup__button-close_type_edit-profile');  // кнопка закрытия попапа профиля
 const buttonCloseNewCard = document.querySelector('.popup__button-close_type_add-card');    // кнопка закрытия попапа доб. карточки
-const buttonCloseZoom = document.querySelector('.popup__button-close_type_zoom-photo')
+const buttonCloseZoom = document.querySelector('.popup__button-close_type_zoom-photo');
 const formProfile = document.querySelector('.popup__form_type_edit-profile');   // форма профиля
 const formNewCard = document.querySelector('.popup__form_type_add-card');
 const profileName = content.querySelector('.profile__name');
@@ -46,24 +46,21 @@ const cardsTemplate = document.querySelector('.cards-template').content;    //  
 
 
 function createCard(item) {
-    let cardsItem = cardsTemplate.querySelector('.cards__grid-item').cloneNode(true);   // при добавлении нового эл-та
+    const cardsItem = cardsTemplate.querySelector('.cards__grid-item').cloneNode(true);   // при добавлении нового эл-та
     cardsItem.querySelector('.cards__title').textContent = item.name;                                 // обязательно снова клонировать
     cardsItem.querySelector('.cards__photo').src = item.link;
 
-    const buttonLike = cardsItem.querySelector('.cards__like');
-    buttonLike.addEventListener('click', () => {
-        buttonLike.classList.toggle('cards__like_active');
+    cardsItem.querySelector('.cards__like').addEventListener('click', evt => {          // повесили лайк
+        evt.target.classList.toggle('cards__like_active');
     })
 
-    const buttonRemove = cardsItem.querySelector('.cards__trash');
-    buttonRemove.addEventListener('click', () => {
-        buttonRemove.closest('.cards__grid-item').remove()
+    cardsItem.querySelector('.cards__trash').addEventListener('click', evt => {        // повесили урну
+        evt.target.closest('.cards__grid-item').remove();
     })
 
-    const buttonZoom = cardsItem.querySelector('.cards__photo');
-    const titleZoom = cardsItem.querySelector('.cards__title');
-    buttonZoom.addEventListener('click', () => {
-        popupZoomPhoto.querySelector('.popup__photo').src = buttonZoom.src;
+    const titleZoom = cardsItem.querySelector('.cards__title');                                     // добавили зум на карточки
+    cardsItem.querySelector('.cards__photo').addEventListener('click', evt => {
+        popupZoomPhoto.querySelector('.popup__photo').src = evt.target.src;
         popupZoomPhoto.querySelector('.popup__photo').classList.add('popup__photo_active');
         popupZoomPhoto.querySelector('.popup__title-zoom').textContent = titleZoom.textContent;
         openPopupZoomPhoto();
@@ -82,7 +79,7 @@ function addNewCard(item) {
 
 function openPopup(popup) {
     popup.classList.add('popup_active');
-    document.querySelector('.body').classList.add('body_scroll-off')
+    document.querySelector('.body').classList.add('body_scroll-off');
 }
 
 function openPopupProfile () {   //  открываем попап и заполняем инпуты данными из профиля
@@ -131,8 +128,6 @@ function formSubmitHandlerNewCard (evt) {
     newCard.name = inputPlace.value;
     newCard.link = inputLink.value;
     addNewCard(newCard);
-    inputPlace.value = '';
-    inputLink.value = '';
     closePopupNewCard()
 }
 
